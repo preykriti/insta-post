c454bcde3237de9afc3ecd0a74d6b231c13a87bf
preykriti <prakriteeshrestha2@gmail.com>
2025-06-12 07:57:26 +0000

    edit and delete funciton

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,26 @@ class PostsController < ApplicationController
         end
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update 
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        if @post.valid?
+            redirect_to root_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to root_path
+    end
+
     private
 
     def post_params
